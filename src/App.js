70e2cce95258c1aef31eee8b1e78a0c51ba07c41
GreywindGit <GreywindGit@users.noreply.github.com>
2018-01-16 12:37:58 +0100

    Add event handlers

--- src/App.js
+++ src/App.js
@@ -1,11 +1,36 @@
 import React, { Component } from 'react';
 import './App.css';
+import UserOutput from './UserOutput/UserOutput';
+import UserInput from './UserInput/UserInput';
 
 class App extends Component {
+  state = {
+    username: 'R2-D2'
+  }
+
+  changeCharacterNameHandler = (newName) => {
+    this.setState({ username: newName });
+  }
+
+  inputNewNameHandler = (event) => {
+    this.setState({ username: event.target.value });
+  }
+
   render() {
     return (
       <div className="App">
-
+        <UserOutput 
+          username={this.state.username}
+          click={this.changeCharacterNameHandler.bind(this, "Luke Skywalker")}/>
+        <UserOutput 
+          username={this.state.username}
+          click={this.changeCharacterNameHandler.bind(this, "Leia Organa")}/>
+        <UserOutput 
+          username={this.state.username}
+          click={this.changeCharacterNameHandler.bind(this, "Han Solo")}/>
+        <UserInput 
+          username={this.state.username}
+          change={this.inputNewNameHandler}/>
       </div>
     );
   }
