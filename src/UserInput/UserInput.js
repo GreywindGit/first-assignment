a3f96763d9b6252bfa75a5f2c073a9cf62b9f846
GreywindGit <GreywindGit@users.noreply.github.com>
2018-01-16 12:37:47 +0100

    Add component content

--- src/UserInput/UserInput.js
+++ src/UserInput/UserInput.js
@@ -1,9 +1,12 @@
 import React from 'react';
+import './UserInput.css';
 
 const userInput = (props) => {
     return (
-        <div>
-            
+        <div className="UserInput">
+            <input type="text" placeholder={props.username} onChange={props.change} />
         </div>
     )
 }
+
+export default userInput;
--- src/UserOutput/UserOutput.js
+++ src/UserOutput/UserOutput.js
@@ -1,1 +1,17 @@
 import React from 'react';
+
+const userOutput = (props) => {
+    const style = {
+        fontWeight: "bold",
+        color: "blueviolet"
+    };
+
+    return (
+        <div>
+            <p>Long time ago, in a galaxy far-far away...</p>
+            <p onClick={props.click}>... there was a hero, called <span style={style}>{props.username}</span>.</p>
+        </div>
+    )
+}
+
+export default userOutput;
